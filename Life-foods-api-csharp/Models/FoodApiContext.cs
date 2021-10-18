7f0984a4cbd62680a29f4948b3c3de573e84ef41
Brian Thorning Jørgensen <brian@thornings.dk>
2021-10-18 16:47:16 +0200

    add Context

---
+++ Life-foods-api-csharp/Models/FoodApiContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Life_foods_api_csharp.Models
+{
+    public class FoodApiContext : DbContext
+    {
+    }
+}
